eec50c61570fa7427e0e39ca26c04e2dde937e73
Alexander Essenwanger <cibernop@mable-anex.de>
2024-08-16 17:38:05 +0200

    workflow and entrypoint

--- docker/entrypoint-offline.sh
+++ docker/entrypoint-offline.sh
@@ -1,4 +1,33 @@
 #!/bin/sh
 
+# Run IG Publisher
+# parts taken from https://github.com/HL7/ig-publisher-scripts/blob/main/_genonce.sh
+
+#!/bin/bash
+publisher_jar=publisher.jar
+input_cache_path=/root/
+echo Checking internet connection...
+curl -sSf tx.fhir.org > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Online"
+    txoption=""
+else
+    echo "Offline"
+    txoption="-tx n/a"
+fi
+
+echo "$txoption"
+
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
+
 PROJECT_DIR=$1
 cd $PROJECT_DIR
+
+publisher=$input_cache_path/$publisher_jar
+if test -f "$publisher"; then
+    java -jar $publisher -ig . -no-sushi $txoption $*
+
+else
+    echo IG Publisher NOT FOUND. Aborting...
+fi
--- docker/entrypoint-online.sh
+++ docker/entrypoint-online.sh
@@ -1,37 +1,46 @@
 #!/bin/sh
 
-PROJECT_DIR=$1
-cd $PROJECT_DIR
-
-# Get publisher
+# Get IG Publisher
 # parts taken from https://github.com/HL7/ig-publisher-scripts/blob/main/_updatePublisher.sh
+
 pubsource=https://github.com/HL7/fhir-ig-publisher/releases/latest/download/
 publisher_jar=publisher.jar
 dlurl=$pubsource$publisher_jar
 
-input_cache_path=$PWD/input-cache/
+input_cache_path=/root/
 
 skipPrompts=false
 FORCE=false
 
 if ! type "curl" > /dev/null; then
-	echo "ERROR: Script needs curl to download latest IG Publisher. Please install curl."
-	exit 1
+    echo "ERROR: Script needs curl to download latest IG Publisher. Please install curl."
+    exit 1
 fi
 
 echo "Checking internet connection"
 curl -sSf tx.fhir.org > /dev/null
 
 if [ $? -ne 0 ] ; then
-  echo "Offline (or the terminology server is down), unable to update.  Exiting"
-  exit 1
+    echo "Offline (or the terminology server is down), unable to update.  Exiting"
+    exit 1
 fi
 
-if [ ! -d "$input_cache_path" ] ; then
-  mkdir ./input-cache
-fi
-
-publisher="/root/$publisher_jar"
+publisher=$input_cache_path/$publisher_jar
 
 echo "Downloading most recent publisher - it's ~100 MB, so this may take a bit"
 curl -L $dlurl -o "$publisher" --create-dirs
+
+
+# Run IG Publisher
+# parts taken from https://github.com/HL7/ig-publisher-scripts/blob/main/_genonce.sh
+
+export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
+
+PROJECT_DIR=$1
+cd $PROJECT_DIR
+
+if test -f "$publisher"; then
+    java -jar $publisher -ig . -no-sushi $*
+else
+    echo IG Publisher NOT FOUND. Aborting...
+fi
