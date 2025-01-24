3d557bfb3d87edc7d85cc6f899de0c2634d8e42a
Alexander Essenwanger <a.essenwanger@gematik.de>
2025-01-24 20:29:17 +0100

    update building script

--- docker/scripts/build-fhir-ig.sh
+++ docker/scripts/build-fhir-ig.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 if [ -z "$IGPUB_VERSION" ]; then
-    IGPUB_VERSION="1.8.8"
+    IGPUB_VERSION=1.8.8
+fi
+
+if [ -n "${SUSHI_VERSION}" ]; then
+    SUSHI=-sushi-${SUSHI_VERSION}
+fi
+
+if [ -z "$OUTPUT_DIR" ]; then
+    OUTPUT_DIR=$PWD/output
+fi
+
+if [ ! -d "$OUTPUT_DIR" ] ; then
+  mkdir -p $OUTPUT_DIR
 fi
 
 if [ -z "$1" ]; then
@@ -10,11 +22,10 @@ else
     PROJECT_DIR=$1
 fi
 
-BASE_OS="ubuntu"
-USER_SETTING="-u $(id -u):$(id -g)"
-PROJECT_MOUNT="-v ${PROJECT_DIR}:/project"
-IMAGE="ghcr.io/cybernop/build-fhir-ig:$IGPUB_VERSION-$BASE_OS"
-# DOCKER_OPTS="--pull always"
-DOCKER_OPTS=""
+IMAGE=ghcr.io/cybernop/build-fhir-ig:$IGPUB_VERSION$SUSHI
 
-docker run --rm $USER_SETTING $PROJECT_MOUNT $DOCKER_OPTS $IMAGE
+PROJECT_MOUNT=${PROJECT_DIR}:/project
+FHIR_CACHE_MOUNT=${HOME}/.fhir/packages:/root/.fhir/packages
+OUTPUT_MOUNT=${OUTPUT_DIR}:/output
+
+docker run --rm -v $PROJECT_MOUNT -v $FHIR_CACHE_MOUNT -v $OUTPUT_MOUNT --pull always $IMAGE
