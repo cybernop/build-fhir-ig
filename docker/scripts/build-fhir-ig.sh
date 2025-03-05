2adb774a3875eda5b814c6e0c800fc3ef9ebef05
Alexander Essenwanger <a.essenwanger@gematik.de>
2025-03-05 10:57:14 +0100

    fix handling of some arguments

--- docker/scripts/build-fhir-ig.sh
+++ docker/scripts/build-fhir-ig.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+echo "Building with the following configuration:"
 if [ -z "$IGPUB_VERSION" ]; then
     IGPUB_VERSION=1.8.8
 fi
+echo "IG Publisher: $IGPUB_VERSION"
 
-if [ -n "${SUSHI_VERSION}" ]; then
+if [ ! -z "${SUSHI_VERSION}" ]; then
     SUSHI=-sushi-${SUSHI_VERSION}
+    echo "FSH Sushi: $SUSHI_VERSION"
+else
+    SUSHI=
 fi
 
 if [ -z "$OUTPUT_DIR" ]; then
@@ -15,24 +20,28 @@ if [ -z "$OUTPUT_DIR" ]; then
     mkdir -p $OUTPUT_DIR
     fi
 fi
+echo "output dir: $OUTPUT_DIR"
 
 if [ -z "$1" ]; then
     PROJECT_DIR=$PWD
 else
     PROJECT_DIR=$1
 fi
+echo "project dir: $PROJECT_DIR"
 
 if [ -z "$PUBLISH_URL" ]; then
     PUBLISH="PUBLISH_URL="
 else
     PUBLISH="PUBLISH_URL=$PUBLISH_URL"
+    echo "publish url: $PUBLISH_URL"
 fi
 
 if [ -z "$JAVA_OPTS" ]; then
-    JAVA_OPTIONS="_JAVA_OPTIONS=-Xmx2g"
-else
-    JAVA_OPTIONS="_JAVA_OPTIONS=$JAVA_OPTS"
+    JAVA_OPTS="-Xmx2g"
 fi
+
+JAVA_OPTIONS="_JAVA_OPTIONS=$JAVA_OPTS"
+printf "Java options: $JAVA_OPTS\n\n\n"
 
 IMAGE=cybernop/build-fhir-ig:$IGPUB_VERSION$SUSHI
 
